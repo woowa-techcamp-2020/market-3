64d815eeadbcf94fbe64cbed79ed3e2ad13be3ed
jungcome7 <jungcome7@gmail.com>
2020-07-08 16:30:50 +0900

    [feature] 비밀번호 입력창을 구현하라
    -비밀번호 입력창 구현 및 리팩토링

---
+++ public/js/utils/domControl.js
@@ -0,0 +1,34 @@
+function generateNodeElement() {
+  return document.createElement("p");
+}
+
+function makeWarningNode(nodeElement, inputFormNode, msg) {
+  writeMsgInNodeElement(nodeElement, msg);
+  addClassInNodeElement(nodeElement, "warning-text");
+  addClassInNodeElement(inputFormNode, "warning");
+}
+5;
+function makeSuccessNode(nodeElement, inputFormNode, msg) {
+  writeMsgInNodeElement(nodeElement, msg);
+  addClassInNodeElement(nodeElement, "success-text");
+  removeClassInNodeElement(inputFormNode, "warning");
+}
+
+function writeMsgInNodeElement(nodeElement, msg) {
+  nodeElement.innerHTML = msg;
+  return nodeElement;
+}
+
+function removeClassInNodeElement(targetNode, className) {
+  targetNode.classList.remove(className);
+}
+
+function addClassInNodeElement(targetNode, className) {
+  targetNode.classList.add(className);
+}
+
+function appendNodeMsg(nodeElement, parentNodeElement) {
+  return parentNodeElement.lastElementChild.tagName === "P"
+    ? parentNodeElement.replaceChild(nodeElement, parentNodeElement.lastElementChild)
+    : parentNodeElement.appendChild(nodeElement);
+}
