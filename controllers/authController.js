22852f0947a0704e7c8521ebbe3022ad87244ae0
Pigrabbit <donghyuk1003@gmail.com>
2020-07-10 01:31:59 +0900

    [layout] pug 페이지들을 연결하라
    아래 연결을 구현했습니다.
    - main -> signUp
    - main -> signIn
    - signIn -> main
    - signIn -> signUp
    - signUp -> signUpSuccess
    - signUpSuccess -> main
    
    로그아웃 기능을 위한 버튼을 구현했습니다.

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,16 +8,19 @@ async function login(req, res) {
 
   if (isValid) {
     res.cookie("id", id, { path: "/" });
-    res.status(200).send("login Success");
-    // res.redirect("../..");
+    res.status(200).redirect("../..");
   } else {
-    res.status(401).send("login Fail");
+    res
+      .status(401)
+      .send(
+        '<script type="text/javascript">alert("아이디 또는 비밀번호를 확인해주세요");window.location.href = "/signin";</script>'
+      );
   }
 }
 
 async function logout(req, res) {
   res.clearCookie("id");
-  res.status(200).send("successfully logout");
+  res.status(200).redirect("../..");
 }
 
 module.exports = {
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,52 @@
-const cookie = document.cookie;
-const [ _ , id ] = cookie.split('=')
-
-
 function hideGreeting() {
   const greeting = document.querySelector(".greeting");
   greeting.classList.add("hide");
 }
 
+function hideLogInButton() {
+  const logInButton = document.querySelector(".login-btn");
+  logInButton.classList.add("hide");
+}
+
+function hideLinks() {
+  const links = document.querySelector(".links");
+  links.classList.add("hide");
+}
+
+function paintLogOutButton() {
+  const logOutButton = document.querySelector('.logout-btn');
+  logOutButton.classList.remove("hide");
+}
+
+function handleLoginButtonClick(e) {
+  window.location.href = '/signin';
+}
+
+function handleLogOutButtonClick(e) {
+  window.location.href = '/auth/logout'
+}
+
+function handleSignUpLinkClick(e) {
+  window.location.href = '/signup';
+}
+
+const logInButton = document.querySelector(".login-btn");
+const logOutButton = document.querySelector(".logout-btn");
+const signUpLink = document.querySelector(".links__signup");
+
+logInButton.addEventListener('click', handleLoginButtonClick);
+logOutButton.addEventListener('click', handleLogOutButtonClick);
+signUpLink.addEventListener('click', handleSignUpLinkClick);
+
 function init() {
-  if (id) hideGreeting();
+  const isLogin = checkCookie('id');
+  
+  if (isLogin) {
+    hideGreeting();
+    hideLogInButton();
+    hideLinks();
+    paintLogOutButton();
+  }
 }
 
 init();
