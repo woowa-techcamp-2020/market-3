7546e1cf8895c73ba44fd23bf5bdcbfc32e226dc
jungcome7 <jungcome7@gmail.com>
2020-07-10 05:02:55 +0900

    [feature] 휴대폰 인증 입력창을 구현하라
    - 휴대폰번호 유효성 검사 구현
    - 휴대폰번호 인증 모달창 구현
    - 인증번호 시간 제한 구현

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -3,9 +3,16 @@ const passwordFormInput = document.querySelector(".password-form__input");
 const passwordCheckFormInput = document.querySelector(".password-check-form__input");
 const emailDomainSelector = document.querySelector(".email-domain-selector__list");
 const nameFormInput = document.querySelector(".name-form__input");
+const mobileFormInput = document.querySelector(".mobile-form__input");
+const mobileFormmodal = document.querySelector('.mobile-form__modal');
+const mobileFormValidBtn = document.querySelector(".mobile-form__valid-btn");
+const mobileFormCloseBtn = document.querySelector(".close");          
 
 idFormInput.addEventListener("blur", idInputHandler);
 passwordFormInput.addEventListener("blur", passwordInputHandler);
 passwordCheckFormInput.addEventListener("blur", passwordChecker);
 emailDomainSelector.addEventListener("change", emailDomainHandler);
 nameFormInput.addEventListener("blur", nameInputHandler);
+mobileFormInput.addEventListener("blur", mobileInputHandler);
+mobileFormValidBtn.addEventListener("click", modalHandler);
+mobileFormCloseBtn.addEventListener("click", closeHandler);
