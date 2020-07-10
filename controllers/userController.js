41fde6b095a003890055e28b98edeaf6c23c9661
Pigrabbit <donghyuk1003@gmail.com>
2020-07-10 11:22:26 +0900

    [feature] 회원가입 페이지 가입완료 버튼을 구현하라
    - 회원가입 버튼을 눌렀을 때, db에 회원정보가 추가되도록 구현했습니다.

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,16 +6,27 @@ const bcrypt = require("bcrypt");
 
 async function createUser(req, res) {
   const hashedPassword = await generateHashedPassword(req.body.password);
+
+  const email = req.body.emailId + "@" + req.body.emailDomain;
+  const address = req.body.address
+    ? req.body.address + " " + req.body.addressDetail
+    : null;
+
   
-  // Todo: pass email, phone and other data as well
   const userObj = {
     id: req.body.id,
     password: hashedPassword,
+    email: email,
+    name: req.body.name,
+    phone: req.body.phone,
+    zipcode: req.body.zipcode,
+    address: req.body.address,
   };
+
   try {
     await insertUser(userObj);
     // Todo: set Cookies for new registerd user
-    res.status(200).redirect('../../signupsuccess');
+    res.status(200).redirect("../../signupsuccess");
   } catch {
     res.status(500).send("user has not been created");
   }
