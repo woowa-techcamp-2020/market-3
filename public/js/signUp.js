f8c10248fed470d84b84d0534b11856f334d02b6
Pigrabbit <donghyuk1003@gmail.com>
2020-07-10 13:16:07 +0900

    [hotfix] 회원가입페이지의  휴대폰 인증 버튼 버그 수정

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -22,7 +22,6 @@ const passwordCheckFormInput = document.querySelector(".password-check-form__inp
 const emailDomainSelector = document.querySelector(".email-domain-selector__list");
 const nameFormInput = document.querySelector(".name-form__input");
 const mobileFormInput = document.querySelector(".mobile-form__input");
-const mobileFormModal = document.querySelector('.mobile-form__modal');
 const mobileFormCloseBtn = document.querySelector(".mobile-form_modal-close");
 const mobileFormValidBtn = document.querySelector(".mobile-form__valid-btn");
 const addressFormCheckbox = document.querySelector(".option-check__checkbox");    
@@ -37,21 +36,30 @@ const agreementFormOptional = document.querySelector('.address-form-optional-che
 const completeBtn = document.querySelector('.complete-btn');
 
 idFormInput.addEventListener("blur", idInputHandler);
+
 passwordFormInput.addEventListener("blur", passwordInputHandler);
 passwordCheckFormInput.addEventListener("blur", passwordChecker);
+
 emailDomainSelector.addEventListener("change", emailDomainHandler);
+
 nameFormInput.addEventListener("blur", nameInputHandler);
+
 mobileFormInput.addEventListener("blur", mobileInputHandler);
 mobileFormValidBtn.addEventListener("click", mobileModalHandler);
 mobileFormCloseBtn.addEventListener("click", mobileModalcloser);
+
 addressFormCheckbox.addEventListener("change", addressInputChecker);
+
 zipcodeInputForm.addEventListener("click", addressModalHandler);
+
 addressInputForm.addEventListener("click", addressModalHandler);
 addressInputFormDetail.addEventListener("click", addressModalHandler);
 addressSearchBtn.addEventListener("click", addressModalHandler);
 addressFormModal.addEventListener("click", addressModalcloser);
+
 agreementFormAll.addEventListener("change", agreementFormAllHandler);
 agreementFormMust.addEventListener("change", agreementFormHandler);
 agreementFormOptional.addEventListener("change", agreementFormHandler);
+
 completeBtn.addEventListener("click", signUpFailHandler);
 
