e4537b46fc5f5adcbe9074f59d7c55c12bcc636b
Pigrabbit <donghyuk1003@gmail.com>
2020-07-10 13:02:24 +0900

    [devOps] Heroku를 통해 배포하라

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
